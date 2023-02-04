9596721e722ff2aae9ede63848996952878f1f1a
akshaykumarrai <akshaykumarrai.1rn16ec014@gmail.com>
2023-02-04 22:24:08 +0530

    excel to pdf

--- main.py
+++ main.py
@@ -6,12 +6,52 @@ from pathlib import Path
 filepaths = glob.glob("*.xlsx")
 for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name="Sheet 1")
+
     pdf =FPDF(orientation="P",unit="mm",format="A4")
     pdf.add_page()
+
     filename=Path(filepath).stem
-    invoice_nr= filename.split("-")[0]
+    invoice_nr,date= filename.split("-")
+
+
     pdf.set_font(family="Times",size=16,style="B")
-    pdf.cell(w=50,h=8,txt=f"Invoice Number:{invoice_nr}")
+    pdf.cell(w=50,h=8,txt=f"Invoice Number:{invoice_nr}",ln=1)
+
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date:{date}",ln=2)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    col= list(df.columns)
+    col= [items.replace("_"," ") for items in col]
+    pdf.set_font(family="Times", size=10,style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=col[0], border=1)
+    pdf.cell(w=70, h=8, txt=col[1], border=1)
+    pdf.cell(w=30, h=8, txt=col[2], border=1)
+    pdf.cell(w=30, h=8, txt=col[3], border=1)
+    pdf.cell(w=30, h=8, txt=col[4], border=1,ln=1)
+    #print(df)
+    i=1
+    for index,row in df.iterrows():
+
+        pdf.set_font(family="Times",size=10)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30,h=8,txt=str(row["product_id"]),border=1)
+        pdf.cell(w=70,h=8,txt=str(row["product_name"]),border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]),border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]),border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]),border=1,ln=1)
+        i=i+1
+
+
+
+
+
+
+
+
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 
