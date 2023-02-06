2b4ae7a567725dd65f8e101900670322062b38a3
akshaykumarrai <akshaykumarrai.1rn16ec014@gmail.com>
2023-02-06 12:42:56 +0530

    completed

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=col[3], border=1)
     pdf.cell(w=30, h=8, txt=col[4], border=1,ln=1)
     #print(df)
-    i=1
+
     for index,row in df.iterrows():
 
         pdf.set_font(family="Times",size=10)
@@ -42,7 +42,25 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]),border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]),border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]),border=1,ln=1)
-        i=i+1
+
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family ="Times", size = 10)
+    pdf.set_text_color(80,80,80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    pdf.set_font(family="Times",size=10)
+    pdf.cell(w=30,h=8,txt=f"The total price is {total_sum}",ln=1)
+
+    pdf.set_font(family="Times",size=10)
+    pdf.cell(w=30,h=8,txt=f"PythonHow")
+
+
+
 
 
 
@@ -53,6 +71,23 @@ for filepath in filepaths:
 
 
     pdf.output(f"PDFs/{filename}.pdf")
+"""
+    from fpdf import FPDF
+ 
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.add_page()
+ 
+content = "
+Lorem ipsum dolor sit amet, consectetur adipiscing 
+elit, sed do eiusmod tempor incididunt ut labore 
+et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco.
+"
+ 
+pdf.set_font(family="Times", size=12)
+pdf.multi_cell(w=0, h=6, txt=content)
+pdf.output("output.pdf")
+    """
 
 
 
